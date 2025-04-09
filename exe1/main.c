3c262b7c476e48ffe9240a40c68cc96418d1ffae
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-04-09 14:25:06 -0300

    ex.01 try 01

--- exe1/main.c
+++ exe1/main.c
@@ -21,11 +21,22 @@ void adc_1_task(void *p) {
         adc_select_input(1); // Select ADC input 1 (GPIO27)
         result = adc_read();
         printf("voltage 1: %f V\n", result * conversion_factor);
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
 
-        // CÓDIGO AQUI
+void adc_2_task(void *p) {
+    adc_init();
+    adc_gpio_init(26);
 
+    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+    const float conversion_factor = 3.3f / (1 << 12);
 
-
+    uint16_t result;
+    while (1) {
+        adc_select_input(1); // Select ADC input 1 (GPIO26)
+        result = adc_read();
+        printf("voltage 2: %f V\n", result * conversion_factor);
         vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
@@ -36,6 +47,7 @@ int main() {
     adc_init();
 
     xTaskCreate(adc_1_task, "LED_Task 1", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_2_task, "LED_Task 2", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while (true) {
